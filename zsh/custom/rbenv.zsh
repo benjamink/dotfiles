4e390c35ed354f472ce9ff5008f7df7b821266c1
Benjamin Krein <superbenk@superk.org>
2015-01-31 09:49:26 -0500

    zsh: set RBENV_ROOT

--- zsh/custom/rbenv.zsh
+++ zsh/custom/rbenv.zsh
@@ -4,5 +4,6 @@ if [ ! -d $HOME/.rbenv ]; then
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   echo "Done."
 fi
+export RBENV_ROOT="$HOME/.rbenv"
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
