ff12337e782f174073d77f540565bf158a896e59
Benjamin Krein <superbenk@superk.org>
2015-01-31 10:07:39 -0500

    add more progress output to setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-[ -h ~/.vimrc ] || ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-[ -h ~/.vim ] || ln -sf ~/dotfiles/vim/.vim ~/.vim
-[ -h ~/.zshrc ] || ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-[ -h ~/.oh-my-zsh ] || ln -sf ~/dotfiles/zsh ~/.oh-my-zsh
+# Setup symlinks
+[ -h ~/.vimrc ] || echo "Setting up ~/.vimrc"; ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+[ -h ~/.vim ] || echo "Setting up ~/.vim"; ln -sf ~/dotfiles/vim/.vim ~/.vim
+[ -h ~/.zshrc ] || echo "Setting up ~/.zshrc"; ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+[ -h ~/.oh-my-zsh ] || echo "Setting up ~/.oh-my-zsh"; ln -sf ~/dotfiles/zsh ~/.oh-my-zsh
 
-if [ "$(ls ~/dotfiles/vim/.vim/bundle)" = "" ]
+# Install Vim Bundles
+if [ -z "$(ls ~/dotfiles/vim/.vim/bundle)" ]
 then
+  echo "Setting up Vim Bundles"
   ~/dotfiles/vim/.vim/update_bundles
+else
+  echo "Bundles already installed"
 fi
