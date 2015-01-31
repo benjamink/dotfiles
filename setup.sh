77db09f38bb74529eed8c156035949e3940baf2f
Benjamin Krein <superbenk@superk.org>
2015-01-31 10:10:41 -0500

    fix symlink check logic

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Setup symlinks
-[ -h ~/.vimrc ] || echo "Setting up ~/.vimrc"; ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-[ -h ~/.vim ] || echo "Setting up ~/.vim"; ln -sf ~/dotfiles/vim/.vim ~/.vim
-[ -h ~/.zshrc ] || echo "Setting up ~/.zshrc"; ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-[ -h ~/.oh-my-zsh ] || echo "Setting up ~/.oh-my-zsh"; ln -sf ~/dotfiles/zsh ~/.oh-my-zsh
+[ -h ~/.vimrc ] || ( echo "Setting up ~/.vimrc"; ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc )
+[ -h ~/.vim ] || ( echo "Setting up ~/.vim"; ln -sf ~/dotfiles/vim/.vim ~/.vim )
+[ -h ~/.zshrc ] || ( echo "Setting up ~/.zshrc"; ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc )
+[ -h ~/.oh-my-zsh ] || ( echo "Setting up ~/.oh-my-zsh"; ln -sf ~/dotfiles/zsh ~/.oh-my-zsh )
 
 # Install Vim Bundles
 if [ -z "$(ls ~/dotfiles/vim/.vim/bundle)" ]
